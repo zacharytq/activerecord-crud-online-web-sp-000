70db6311e68d87d56c75735aed64cf81530def89
chic-page-6502 <zacharytq@gmail.com>
2020-01-10 02:31:47 +0000

    Automatically backed up by Learn

--- app/movies_controller.rb
+++ app/movies_controller.rb
@@ -36,15 +36,15 @@ def can_get_the_first_item_in_the_database
 end
 
 def can_get_the_last_item_in_the_database
-  __
+  Movie.last.title
 end
 
 def can_get_size_of_the_database
-  __
+  Movie.all.size
 end
 
 def can_find_the_first_item_from_the_database_using_id
-  __
+  Movie.find_by(id: 1).title
 end
 
 def can_find_by_multiple_attributes
@@ -52,7 +52,7 @@ def can_find_by_multiple_attributes
   # title == "Title"
   # release_date == 2000
   # director == "Me"
-  __
+  Movie.find(:all, :conditons => {["title = ?", "Title"], ["release_date = 2000"], ['director = "ME"']})
 end
 
 def can_find_using_where_clause_and_be_sorted

e7ad4aa66df489e21d29157a8c9e3db0caab8931
chic-page-6502 <zacharytq@gmail.com>
2020-01-10 01:57:34 +0000

    Automatically backed up by Learn

--- app/movies_controller.rb
+++ app/movies_controller.rb
@@ -20,7 +20,8 @@ def can_be_created_with_a_hash_of_attributes
       lead: "Paul Newman",
       in_theaters: false
   }
-  movie = Movie.create(attributes).save
+  movie = Movie.create(attributes)
+  movie.save
 end
 
 def can_be_created_in_a_block(args = {:title = "Home Alone", :release_date = 1990})

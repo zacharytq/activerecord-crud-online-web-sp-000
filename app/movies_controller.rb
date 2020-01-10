bcd8318baeba21f28ba8bdf587f31968572dc05a
chic-page-6502 <zacharytq@gmail.com>
2020-01-10 01:59:26 +0000

    Automatically backed up by Learn

--- app/movies_controller.rb
+++ app/movies_controller.rb
@@ -24,7 +24,7 @@ def can_be_created_with_a_hash_of_attributes
   movie.save
 end
 
-def can_be_created_in_a_block(args = {:title = "Home Alone", :release_date = 1990})
+def can_be_created_in_a_block(args = {:title = "Home Alone" :release_date = 1990})
   # If no arguments are passed, use default values:
   # title == "Home Alone"
   # release_date == 1990

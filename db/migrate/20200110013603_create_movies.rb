c7056a9eb2c30e3691a04b6e3425c7efac4ce8e5
chic-page-6502 <zacharytq@gmail.com>
2020-01-10 01:42:41 +0000

    Automatically backed up by Learn

--- db/migrate/20200110013603_create_movies.rb
+++ db/migrate/20200110013603_create_movies.rb
@@ -1,6 +1,6 @@
 class CreateMovies < ActiveRecord::Migration[5.2]
   def change
-    create_table :create_movies do |t|
+    create_table :movies do |t|
       t.string :title
       t.integer :release_date
       t.string :director

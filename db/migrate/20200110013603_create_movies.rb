4f851866593c77f2be133f13b71a0e2aba3546cb
chic-page-6502 <zacharytq@gmail.com>
2020-01-10 01:42:10 +0000

    Automatically backed up by Learn

--- db/migrate/20200110013603_create_movies.rb
+++ db/migrate/20200110013603_create_movies.rb
@@ -5,6 +5,7 @@ class CreateMovies < ActiveRecord::Migration[5.2]
       t.integer :release_date
       t.string :director
       t.string :lead
-      t.boolead :in_theaters
+      t.boolean :in_theaters
+    end
   end
 end

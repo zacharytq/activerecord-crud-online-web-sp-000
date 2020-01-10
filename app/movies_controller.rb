35e4fc56e2dabba789240daf0b0ce4f12e3ce1f4
chic-page-6502 <zacharytq@gmail.com>
2020-01-10 02:36:56 +0000

    Automatically backed up by Learn

--- app/movies_controller.rb
+++ app/movies_controller.rb
@@ -52,7 +52,7 @@ def can_find_by_multiple_attributes
   # title == "Title"
   # release_date == 2000
   # director == "Me"
-  Movie.find(:all, :conditons => ["title = ?", "Title"], ["release_date = 2000"], ['director = "ME"'])
+  Movie.find(:all, :conditions => ["title = ?", "Title"])
 end
 
 def can_find_using_where_clause_and_be_sorted
